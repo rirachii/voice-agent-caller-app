30d4fec4824d5076e53bee4221c8db97f66f7941
Michelle Weng <82119639+rirachii@users.noreply.github.com>
2025-03-08 14:13:50 -0500

    Create UI package index.tsx file

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,11 @@
+// Export components
+export * from './components';
+
+// Export hooks
+export * from './hooks';
+
+// Export theme
+export * from './theme';
+
+// Export provider
+export * from './provider';
